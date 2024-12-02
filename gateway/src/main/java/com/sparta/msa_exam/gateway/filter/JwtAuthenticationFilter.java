b7b600616cc4822fcf84947ff1edb14c1ae8957f
jshstar <jshstar97@gmail.com>
2024-12-03 02:26:28 +0900

    fix: 포트 출력 문제 해결

--- gateway/src/main/java/com/sparta/msa_exam/gateway/filter/JwtAuthenticationFilter.java
+++ gateway/src/main/java/com/sparta/msa_exam/gateway/filter/JwtAuthenticationFilter.java
@@ -3,6 +3,8 @@ package com.sparta.msa_exam.gateway.filter;
 import javax.crypto.SecretKey;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
@@ -24,11 +26,34 @@ public class JwtAuthenticationFilter implements GlobalFilter, Ordered {
 
 	@Value("${service.jwt.secret-key}")
 	private String secretKey;
+	private final ReactiveDiscoveryClient discoveryClient;
+
+	public JwtAuthenticationFilter(ReactiveDiscoveryClient discoveryClient) {
+		this.discoveryClient = discoveryClient;
+	}
 
 	@Override
 	public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
 		String path = exchange.getRequest().getURI().getPath();
+
 		if (isPublicPath(path)) {
+			if (path.equals("/api/v1/auth/signIn") || path.equals("/api/v1/auth/signUp")) {
+				return discoveryClient.getInstances("AUTH-SERVICE").collectList()
+					.flatMap(instances -> {
+						if (instances.isEmpty()) {
+							log.error("No instances AUTH-SERVICE");
+							return Mono.error(new IllegalStateException("No instances AUTH-SERVICE"));
+						}
+
+						ServiceInstance chosenInstance = instances.get(0);
+						String port = String.valueOf(chosenInstance.getUri().getPort());
+
+						exchange.getResponse().getHeaders().add("Server-Port", port);
+						log.info("Server-Port {}: {}", path, port);
+
+						return chain.filter(exchange);
+					});
+			}
 			return chain.filter(exchange);
 		}
 
@@ -39,10 +64,8 @@ public class JwtAuthenticationFilter implements GlobalFilter, Ordered {
 			return exchange.getResponse().setComplete();
 		}
 
-
 		return chain.filter(exchange);
 	}
-
 	private String extractToken(ServerWebExchange exchange) {
 		String authHeader = exchange.getRequest().getHeaders().getFirst("Authorization");
 		if (authHeader != null && authHeader.startsWith("Bearer ")) {
